f48466ad7ecaccea0ecc7f6a7d9702dd67319fa8
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-03-29 18:48:29 +0800

    add double features review

---
+++ src/com/zyh/test/j2se/lang/number/DoubleValue.java
@@ -0,0 +1,19 @@
+package com.zyh.test.j2se.lang.number;
+
+/**
+ * double type features
+ * 
+ * @author zhyhang
+ *
+ */
+public class DoubleValue {
+
+	public static void main(String[] args) {
+
+		System.out.format(
+				"Double Max Value\n[E format:%ef]\n[Normal format:%f]\n",
+				Double.MAX_VALUE, Double.MAX_VALUE);
+
+	}
+
+}
