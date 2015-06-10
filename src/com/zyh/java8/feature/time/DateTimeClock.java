68664d674b817bbd7e4c49ccb84b71407c63a7ba
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-06-10 11:00:31 +0800

    java8 date time try

---
+++ src/com/zyh/java8/feature/time/DateTimeClock.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.zyh.java8.feature.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author zhyhang
+ *
+ */
+public class DateTimeClock {
+
+	public static void main(String[] args) {
+
+		// Date and time with timezone in Java 8 ZoneId america = ZoneId.of("America/New_York");
+		LocalDateTime localtDateAndTime = LocalDateTime.now();
+		ZonedDateTime dateAndTimeInNewYork = ZonedDateTime.of(localtDateAndTime, ZoneId.of("America/New_York"));
+		System.out.println("Current date and time in a local timezone : " + localtDateAndTime);
+		System.out.println("Current date and time in a particular timezone : " + dateAndTimeInNewYork);
+
+		String dayAfterTommorrow = "20120225";
+		LocalDate formatted = LocalDate.parse(dayAfterTommorrow, DateTimeFormatter.BASIC_ISO_DATE);
+		System.out.printf("Date generated from String %s is %s %n", dayAfterTommorrow, formatted);
+		
+
+	}
+
+}
