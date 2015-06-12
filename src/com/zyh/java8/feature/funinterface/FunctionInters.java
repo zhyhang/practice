90acafed9a6d2fe7d8fe8f81b49e435c8e852dd9
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-06-12 22:19:04 +0800

    add java8 function try

--- src/com/zyh/java8/feature/funinterface/FunctionInters.java
+++ src/com/zyh/java8/feature/funinterface/FunctionInters.java
@@ -3,12 +3,16 @@
  */
 package com.zyh.java8.feature.funinterface;
 
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -19,29 +23,43 @@ public class FunctionInters {
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 
+		// optional
 		Optional<String> name = Optional.of("zhao");
 		System.out.println(name.orElse("luan"));
-
 		System.out.println(Arrays.asList("1a", "1b", "1c").stream().allMatch(s -> s.startsWith("1")));
 
+		// file
 		Path path = Paths.get("name.txt");
-		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-			writer.write("hello");
-			writer.newLine();
-			writer.write(" world!");
+		try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path))) {
+			writer.println("hello");
+			writer.println(" world!");
 		} catch (Exception e) {
 		}
-
 		try (Stream<String> stream = Files.lines(Paths.get("name.txt"))) {
 			stream.map(String::trim).forEach(System.out::println);
 		} catch (Exception e) {
 		}
-		
 		System.out.println(Paths.get("name.txt").toFile().getCanonicalPath());
+
+		// map
+		ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<Integer, String>();
+		IntStream.range(1, 100).forEach(i -> {
+			map.put(i, "no.".concat(Objects.toString(i)));
+		});
+		map.forEach(1, (key, value) -> System.out.printf("key: %s; value: %s; thread: %s\n", key, value, Thread
+				.currentThread().getName()));
+
+		String result = map.reduce(1, (key, value) -> {
+			return key + "=" + value;
+		}, (s1, s2) -> {
+			return s1 + ", " + s2;
+		});
+
+		System.out.println("Result: " + result);
 
 	}
 
