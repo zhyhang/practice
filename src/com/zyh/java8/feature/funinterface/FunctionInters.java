7ca6d91a1deec16c1070fc56bfb6d8f1279693d3
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-06-27 23:28:55 +0800

    add optional usage

--- src/com/zyh/java8/feature/funinterface/FunctionInters.java
+++ src/com/zyh/java8/feature/funinterface/FunctionInters.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -50,17 +49,43 @@ public class FunctionInters {
 		IntStream.range(1, 100).forEach(i -> {
 			map.put(i, "no.".concat(Objects.toString(i)));
 		});
-		map.forEach(1, (key, value) -> System.out.printf("key: %s; value: %s; thread: %s\n", key, value, Thread
-				.currentThread().getName()));
+		map.forEach(1, (key, value) -> System.out.printf("key: %s; value: %s; thread: %s\n", key, value,
+				Thread.currentThread().getName()));
 
 		String result = map.reduce(1, (key, value) -> {
 			return key + "=" + value;
-		}, (s1, s2) -> {
+		} , (s1, s2) -> {
 			return s1 + ", " + s2;
 		});
 
 		System.out.println("Result: " + result);
 
+		// Optional usage for avoiding null check
+		Child child = new Child();
+		child.setParent(new Parent());
+		Optional.ofNullable(child).map(Child::getParent).ifPresent(System.out::println);
+
 	}
 
+}
+
+class Parent {
+
+	public String toString() {
+		return "Children's parent";
+	}
+
+}
+
+class Child {
+
+	private Parent parent;
+
+	public Parent getParent() {
+		return this.parent;
+	}
+
+	public void setParent(Parent parent) {
+		this.parent = parent;
+	}
 }
