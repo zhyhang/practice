9551f0cb68575ae8c7b99c1b2f4bc43172096d63
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-11-13 20:26:08 +0800

    upgrade undertow version, add double chronicle map replication service

---
+++ src/com/zyh/test/chronicle/DualMasterMapService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.zyh.test.chronicle;
+
+/**
+ * double chronicle map replication, receiving remote put data
+ * @author zhyhang
+ *
+ */
+public class DualMasterMapService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+	
+	
+
+}
+
+
+class RequestHandler{
+	
+}
