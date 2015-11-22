31adb43b6e53e7fc89c00b543f4593f20c695103
yuhang.zhao <yuhang.zhao@ipinyou.com>
2015-11-22 18:01:45 +0800

    enhance byte to long test

--- src/com/zyh/test/j2se/lang/number/ByteToLong.java
+++ src/com/zyh/test/j2se/lang/number/ByteToLong.java
@@ -4,6 +4,7 @@
 package com.zyh.test.j2se.lang.number;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhyhang
@@ -15,22 +16,35 @@ public class ByteToLong {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// pos(0) is a type, next 8 byte is long value
-		ByteBuffer bb = ByteBuffer.allocate(9);
-		bb.position(1);
-		long lv = 0x1980ffff;
+		ByteBuffer bb = ByteBuffer.allocate(8);
+		byte[] bbbs=bb.array();
+//		long lv = -0xffff000000001l;
+		long lv= 0xffff;
 		bb.putLong(lv);
-		long plv = byteToLong(bb.array());
-		System.out.format("orig long value/parse long vlue: %d/%d", lv, plv);
+		long timeBb=0;
+		long timeBtoL=0;
+		long plv = 0;
+		for (int i = 0; i < 100000000; i++) {
+			long tsb = System.nanoTime();
+			bb.position(0);
+			plv = bb.getLong();
+			timeBb+=System.nanoTime() - tsb;
+			tsb=System.nanoTime();
+			plv = byteToLong(bbbs);
+			timeBtoL+=System.nanoTime() - tsb;
+		}
+		System.out.format("orig long value/parse long vlue: %d/%d\n", lv, plv);
+		System.out.format("time cost ByteBuffer/byteToLong[%d/%d]ms\n", TimeUnit.NANOSECONDS.toMillis(timeBb), 
+				TimeUnit.NANOSECONDS.toMillis(timeBtoL));
 
 	}
 
-	private static Long byteToLong(byte[] id) {
+	private static long byteToLong(byte[] id) {
 		long v = 0;
-		for (int i = 1; i <= 8; i++) {
-			v |= 0xff00000000000000L >>> (i - 1) * 8 & id[i] << 64 - i * 8;
+		for (int i = 0; i < Long.BYTES; i++) {
+			v |= 0xff00000000000000L >>> i * Byte.SIZE & (long) id[i] << Long.SIZE - (i + 1) * Byte.SIZE;
 		}
-		return Long.valueOf(v);
+		return v;
 	}
 
 }
