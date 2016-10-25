b33b99e4e989d49c8eca3d9156c1288c75db62cb
yuhang.zhao <yuhang.zhao@ipinyou.com>
2016-10-25 17:19:47 +0800

    add transfer timestamp to datetime example

--- src/com/zyh/java8/feature/time/DateTimeClock.java
+++ src/com/zyh/java8/feature/time/DateTimeClock.java
@@ -3,6 +3,7 @@
  */
 package com.zyh.java8.feature.time;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -17,7 +18,8 @@ public class DateTimeClock {
 
 	public static void main(String[] args) {
 
-		// Date and time with timezone in Java 8 ZoneId america = ZoneId.of("America/New_York");
+		// Date and time with timezone in Java 8 ZoneId america =
+		// ZoneId.of("America/New_York");
 		LocalDateTime localtDateAndTime = LocalDateTime.now();
 		ZonedDateTime dateAndTimeInNewYork = ZonedDateTime.of(localtDateAndTime, ZoneId.of("America/New_York"));
 		System.out.println("Current date and time in a local timezone : " + localtDateAndTime);
@@ -26,8 +28,9 @@ public class DateTimeClock {
 		String dayAfterTommorrow = "20120225";
 		LocalDate formatted = LocalDate.parse(dayAfterTommorrow, DateTimeFormatter.BASIC_ISO_DATE);
 		System.out.printf("Date generated from String %s is %s %n", dayAfterTommorrow, formatted);
-		
 
+		// Transfer timestamp to LocalDateTime;
+		LocalDate date = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDate();
 	}
 
 }
