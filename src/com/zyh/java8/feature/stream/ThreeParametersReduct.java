39e298d742464c03e22b458b82b74778f6708b1a
zhyhang <zhyhang@zhyhang-pc2016>
2017-02-19 17:42:09 +0800

    add three parameters collect() test

--- src/com/zyh/java8/feature/stream/ThreeParametersReduct.java
+++ src/com/zyh/java8/feature/stream/ThreeParametersReduct.java
@@ -28,21 +28,43 @@ public class ThreeParametersReduct {
 		List<String> list = Collections.synchronizedList(new ArrayList<>());
 		System.out.println("input list:" + System.identityHashCode(list));
 		System.out.println();
-		List<String> reducedList = IntStream.range(0, 1024).mapToObj(String::valueOf).parallel().unordered()
-				.reduce(list, (l, s) -> {
+		List<String> reducedList = IntStream.range(0, 128).mapToObj(String::valueOf).parallel().unordered().reduce(list,
+				(l, s) -> {
 					l.add(s);
 					return l;
 				}, (l1, l2) -> {
 					if (l1 != l2) {
 						System.out.println("list1 <> list2");
 					}
-					System.out.println("Thtread: " + Thread.currentThread().getId() + "\tlist1: "
+					System.out.println("Thread: " + Thread.currentThread().getId() + "\tlist1: "
 							+ System.identityHashCode(l1) + "\tlist2: " + System.identityHashCode(l2));
 					return l1;
 				});
 		System.out.println();
 		System.out.println("reduced list:" + System.identityHashCode(list));
+		System.out.println("reduced list elements:" + list);
 		System.out.println("reduced list size:" + reducedList.size());
+
+		System.out.println();
+		List<String> list2 = IntStream.range(0, 128).mapToObj(String::valueOf).parallel().unordered().collect(() -> {
+			List<String> nlist = new ArrayList<>();
+			System.out.println("Thread-supplier: " + Thread.currentThread().getId() + "\tnew list: "
+					+ System.identityHashCode(nlist));
+			return nlist;
+		}, (l, s) -> {
+			l.add(s);
+			System.out.println("Thread-accumulator: " + Thread.currentThread().getId() + "\tlist: "
+					+ System.identityHashCode(l));
+		}, (l1, l2) -> {
+			l1.addAll(l2);
+			System.out.println("Thread-combiner: " + Thread.currentThread().getId() + "\tlist1: "
+					+ System.identityHashCode(l1) + "\tlist2: " + System.identityHashCode(l2));
+		});
+		System.out.println();
+		System.out.println("reduced list2:" + System.identityHashCode(list2));
+		System.out.println("reduced list2 elements:" + list2);
+		System.out.println("reduced list2 size:" + list2.size());
+
 	}
 
 }
