d3ef12884fe00dfd6811e37a72c370d37120cc26
zhyhang <zhyhang1978@gmail.com>
2017-11-19 10:22:04 +0800

    add date time and timestamp convert each other

--- src/com/zyh/java8/feature/time/DateTimeClock.java
+++ src/com/zyh/java8/feature/time/DateTimeClock.java
@@ -3,12 +3,15 @@
  */
 package com.zyh.java8.feature.time;
 
+import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * @author zhyhang
@@ -25,12 +28,23 @@ public class DateTimeClock {
 		System.out.println("Current date and time in a local timezone : " + localtDateAndTime);
 		System.out.println("Current date and time in a particular timezone : " + dateAndTimeInNewYork);
 
+		// parse local date time
 		String dayAfterTommorrow = "20120225";
 		LocalDate formatted = LocalDate.parse(dayAfterTommorrow, DateTimeFormatter.BASIC_ISO_DATE);
 		System.out.printf("Date generated from String %s is %s %n", dayAfterTommorrow, formatted);
+		
+		// current millis
+		System.out.println("current millis from Clock: "+Clock.systemDefaultZone().millis());
+		System.out.println("current millis from System: "+System.currentTimeMillis());	
 
 		// Transfer timestamp to LocalDateTime;
-		LocalDate date = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDateTime ts2dt = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+		System.out.println("current millis timestamp to date time: "+ts2dt);
+		
+		// transfer local date time to millis
+		long epochMilli = ts2dt.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+		System.out.println("current millis converted from local date time: "+epochMilli);
+		
 	}
 
 }
